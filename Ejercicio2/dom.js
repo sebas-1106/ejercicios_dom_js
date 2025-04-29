c5c2fc05d09d7b64ea5e4a96930c3072532d4b6e
sebas-1106 <acevedosebastian455@gmail.com>
2025-04-29 19:12:15 +0200

    DOM

---
+++ Ejercicio2/dom.js
@@ -0,0 +1,23 @@
+/*2.1 Inserta dinamicamente en un html un div vacio con javascript.
+
+2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
+
+2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un 
+	loop con javascript.
+
+2.4 Inserta dinamicamente con javascript en un html una p con el 
+	texto 'Soy dinámico!'.
+
+2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+
+2.6 Basandote en el siguiente array crea una lista ul > li con 
+los textos del array.
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+
+2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+
+2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+	Recuerda que no solo puedes insertar elementos con .appendChild.
+
+2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase 
+	.fn-insert-here */
